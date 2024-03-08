2d84187b4cfdc916ea27388d33d7539e9dbe04eb
leannemcabey <leanne.m.cabey@gmail.com>
2024-03-08 17:21:11 -0500

    add user to db

--- server-actions/login.ts
+++ server-actions/login.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { createClient } from "@/utils/supabase/server";
+import {revalidatePath} from "next/cache";
 
 export const handleGoogleSignIn = async (response) => {
     const supabase = createClient();
@@ -15,7 +16,27 @@ export const handleGoogleSignIn = async (response) => {
     }
 
     const { user: { email, identities } } = data;
+    await findOrAddUser(email, identities[0].identity_data.name);
 
-    console.log(email)
-    console.log(identities[0].identity_data.name)
+    revalidatePath('/home');
+
+    return { message: 'Success' };
 };
+
+const findOrAddUser = async (email: string, name: string) => {
+    const supabase = createClient();
+
+    const { data: existingUser, error: selectUserError } = await supabase
+        .from('users')
+        .select('*')
+        .eq('email', email)
+
+    if (!existingUser) {
+        const { data: newUser, error: insertUserError } = await supabase
+            .from('users')
+            .insert([{
+                name,
+                email
+            }])
+    }
+}
