f050bac884ec43ddd3ceb04c76e1c5f240dd1623
leannemcabey <leanne.m.cabey@gmail.com>
2024-07-12 16:12:44 -0400

    add cleanout bag confirmation modal

--- app/components/cleanoutBag/AddOrRemoveFromCleanoutBag.tsx
+++ app/components/cleanoutBag/AddOrRemoveFromCleanoutBag.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { useState } from "react";
 import { addOrRemoveFromCleanoutBag } from "@/app/server-actions/cleanout-bag/addOrRemoveFromCleanoutBag";
 import { Article } from "@/types/Article";
+import Modal from "@/app/components/modal/Modal";
+import CloseModalButton from "@/app/components/modal/CloseModalButton";
 
 interface AddOrRemoveFromCleanoutBagProps {
     article: Article;
@@ -10,26 +12,42 @@ interface AddOrRemoveFromCleanoutBagProps {
 
 const AddOrRemoveFromCleanoutBag = ({ article }: AddOrRemoveFromCleanoutBagProps) => {
     const [inCleanoutBag, setInCleanoutBag] = useState<boolean>(article.inCleanoutBag);
+    const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
 
     const changeCleanoutBagStatus = () => {
         addOrRemoveFromCleanoutBag(article)
             .then(() => setInCleanoutBag(!inCleanoutBag))
+            .then(() => setShowConfirmation(true))
+            .then(() => setTimeout(() => setShowConfirmation(false), 2000))
     }
 
     const position = inCleanoutBag ? "justify-end" : "";
     const toggleStyling = inCleanoutBag ? "bg-theme-blue" : "bg-background-green";
 
     return (
-        <div className={`flex ${position} ${toggleStyling} border border-theme-blue rounded-full w-20 h-max drop-shadow`}>
-            <div className={`h-12 w-12 bg-background-green border border-theme-blue rounded-full p-2 drop-shadow`}>
-                <Image
-                    src={"/broom-icon.png"}
-                    alt={"broom icon"}
-                    width="30" height="30"
-                    onClick={() => changeCleanoutBagStatus()}
-                />
+        <>
+            <div className={`flex ${position} ${toggleStyling} border border-theme-blue rounded-full w-20 h-max drop-shadow`}>
+                <div className={`h-12 w-12 bg-background-green border border-theme-blue rounded-full p-2 drop-shadow`}>
+                    <Image
+                        src={"/broom-icon.png"}
+                        alt={"broom icon"}
+                        width="30" height="30"
+                        onClick={() => changeCleanoutBagStatus()}
+                    />
+                </div>
             </div>
-        </div>
+
+            {showConfirmation &&
+                <Modal setIsOpen={setShowConfirmation}>
+                    <div className="flex flex-col items-center text-center">
+                        <Image unoptimized={true} src="/cleanout.gif" alt="broom sweeping" height="200" width="200"/>
+                        <p className="text-xl mt-4 text-theme-blue">
+                            {inCleanoutBag ? "Added to cleanout bag!" : "Removed from cleanout bag!"}
+                        </p>
+                    </div>
+                </Modal>
+            }
+        </>
     )
 };
 
