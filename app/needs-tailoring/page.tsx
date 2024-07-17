e9578c1a54358852eb53ddb1908c18490924187b
leannemcabey <leanne.m.cabey@gmail.com>
2024-07-17 18:11:43 -0400

    add error handling to tailoring page

--- app/needs-tailoring/page.tsx
+++ app/needs-tailoring/page.tsx
@@ -4,35 +4,43 @@ import Image from "next/image";
 import BackButton from "@/app/components/navigation/BackButton";
 import { getArticlesNeedingTailoring } from "@/app/server-actions/needs-tailoring/getArticlesNeedingTailoring";
 import NeedsTailoringContainer from "@/app/components/needsTailoring/NeedsTailoringContainer";
+import ErrorPageContainer from "@/app/components/ErrorPageContainer";
 
 export default async function NeedsTailoring() {
-    const articles = await getArticlesNeedingTailoring();
+    const { articles, error } = await getArticlesNeedingTailoring();
+
+    const errorMessage = "An error occurred when retrieving your articles. Please go back and try again."
 
     return (
         <Layout>
             <BackButton />
-            <div className="mt-4">
-                <div className="flex justify-center">
-                    <h1 className="text-2xl mb-4 mr-2">needs tailoring</h1>
-                    <div>
-                        <Image
-                            src={"/needle.svg"}
-                            alt={"needle icon"}
-                            width="30"
-                            height="30"
-                        />
+
+            {error && <ErrorPageContainer errorMessage={errorMessage} />}
+
+            {articles && (
+                <div className="mt-4">
+                    <div className="flex justify-center">
+                        <h1 className="text-2xl mb-4 mr-2">needs tailoring</h1>
+                        <div>
+                            <Image
+                                src={"/needle.svg"}
+                                alt={"needle icon"}
+                                width="30"
+                                height="30"
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <div className="mt-1 mb-6 text-center">
-                    <p>
-                        Oftentimes the reason we don't wear certain clothes is simply because they don't fit us right.
-                        Tailoring is a great way to give new life to an item and avoid creating waste!
-                    </p>
-                </div>
+                    <div className="mt-1 mb-6 text-center">
+                        <p>
+                            Oftentimes the reason we don't wear certain clothes is simply because they don't fit us right.
+                            Tailoring is a great way to give new life to an item and avoid creating waste!
+                        </p>
+                    </div>
 
-                <NeedsTailoringContainer articles={articles}/>
-            </div>
+                    <NeedsTailoringContainer articles={articles}/>
+                </div>
+            )}
         </Layout>
     )
 };
