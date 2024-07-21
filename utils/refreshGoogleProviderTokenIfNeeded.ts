b1d020bb8e2a1124da63be9cfb0805dc6fe5da49
leannemcabey <leanne.m.cabey@gmail.com>
2024-07-21 19:37:39 -0400

    fix google token refresh timestamp comparison

--- utils/refreshGoogleProviderTokenIfNeeded.ts
+++ utils/refreshGoogleProviderTokenIfNeeded.ts
@@ -6,9 +6,13 @@ export const refreshGoogleProviderTokenIfNeeded = async () => {
     const supabase = createClient();
 
     const providerTokenExpires = window.localStorage.getItem('expires_at');
-    const isExpired = Date.now() > providerTokenExpires;
+
+    // Get the current timestamp in seconds
+    const now = Math.round(Date.now() / 1000);
+    const isExpired = now > providerTokenExpires;
 
     if (isExpired) {
+        console.log("refreshing google token")
         const refreshToken =  window.localStorage.getItem('oauth_provider_refresh_token');
 
         refreshGoogleProviderToken(refreshToken)
