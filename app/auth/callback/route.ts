ed8f3f13998f671806afd5d5c31698015b9b5b87
leannemcabey <leanne.m.cabey@gmail.com>
2024-07-21 13:30:53 -0400

    cleanup

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from 'next/server'
-// The client you created from the Server-Side Auth instructions
 import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
-    console.log('auth GET')
     const { searchParams, origin } = new URL(request.url)
     const code = searchParams.get('code')
     // if "next" is in param, use it as the redirect URL
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -2,31 +2,25 @@ import { NextResponse, type NextRequest } from 'next/server'
 import { createClient } from "@/utils/supabase/server";
 
 export async function updateSession(req: NextRequest) {
-  console.log('middleware running')
+  let res = NextResponse.next({request: req,})
 
-  let res = NextResponse.next({
-    request: req,
-  })
-
-  // This seems to work whether it's the middlewareClient or server client
   const supabase = createClient()
 
   // IMPORTANT: Avoid writing any logic between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
-  // This is null right after logging in
-  // it only returns a value once you refresh the page
   const { data: { user } } = await supabase.auth.getUser();
 
-  // console.log(`user: ${JSON.stringify(user)}`)
-
+  // If the user is not logged in, and they're trying to reach anything other than the auth callback
+  // or the login page, redirect them to the login page.
   if (!user && !req.nextUrl.pathname.startsWith('/auth/callback') && req.nextUrl.pathname !== '/') {
     const url = req.nextUrl.clone()
     url.pathname = '/'
     return NextResponse.redirect(url)
   }
 
+  // If the user is logged in, and they try to reach the login page, redirect them to the home page
   if (user && req.nextUrl.pathname === '/') {
     console.log("user logged in, redirecting to home")
     const url = req.nextUrl.clone()
