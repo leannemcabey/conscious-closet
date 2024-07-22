0b39a125019b685e73c2a8f55929eda12c1cb76c
leannemcabey <leanne.m.cabey@gmail.com>
2024-07-22 17:35:56 -0400

    add translucent background to modal

--- app/components/modal/Modal.tsx
+++ app/components/modal/Modal.tsx
@@ -13,6 +13,7 @@ const Modal = ({ setIsOpen, children }: ModalProps) => {
     const outsideClickHandler = (event) => {
         event.preventDefault()
         const includesModalElement = event.composedPath().includes(modalRef.current!!);
+
         if (modalRef.current && !includesModalElement) {
             setIsOpen(false)
         }
@@ -28,10 +29,16 @@ const Modal = ({ setIsOpen, children }: ModalProps) => {
     return (
         <>
             {createPortal(
-                <div ref={modalRef} data-testid="modal" className="flex justify-center w-full h-full">
-                    <div className="flex flex-col bg-white px-4 py-4 rounded-md border border-neutral-200 drop-shadow-2xl w-5/6 min-h-80 max-h-[600px] absolute animate-enter-from-bottom">
-                        <div className="mt-6 mx-2">
-                            {children}
+                <div data-testid="translucent-layer" className="absolute top-0 left-0 h-screen w-screen bg-transparent-bg">
+                    <div className="flex justify-center w-full h-full">
+                        <div
+                            ref={modalRef}
+                            data-testid="modal"
+                            className="flex flex-col bg-white px-4 py-4 rounded-md border border-neutral-200 drop-shadow-2xl w-5/6 min-h-80 max-h-[600px] absolute animate-enter-from-bottom"
+                        >
+                            <div className="mt-6 mx-2">
+                                {children}
+                            </div>
                         </div>
                     </div>
                 </div>,
