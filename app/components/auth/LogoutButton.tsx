4dd582dcd9291f80931d7c55bd30b76104cb81b9
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-01 18:23:12 -0400

    side meny responsiveness

--- app/components/auth/LogoutButton.tsx
+++ app/components/auth/LogoutButton.tsx
@@ -1,7 +1,7 @@
 export const LogoutButton = () => (
     <form action="/auth/logout" method="post">
         <button
-            className="py-1 px-2 rounded-md no-underline text-sm text-theme-green border border-theme-green drop-shadow"
+            className="py-1 px-2 rounded-md no-underline text-sm md:text-lg text-text-green border border-theme-green drop-shadow"
         >
             sign out
         </button>
--- app/components/navigation/MenuItem.tsx
+++ app/components/navigation/MenuItem.tsx
@@ -19,22 +19,39 @@ const MenuItem = ({ linkTo, imageSrc, imageAltText, label, subItems }: MenuItemP
     const alt = isOpen ? "collapse" : "expand";
 
     return (
-        <div className="text-lg flex flex-col py-4 border border-dotted border-neutral-300 border-b-2 border-t-0 border-l-0 border-r-0">
+        <div className="text-lg flex flex-col py-4 md:py-6 border border-dotted border-neutral-300 border-b-2 border-t-0 border-l-0 border-r-0">
             <div className="flex space-x-4">
                 {subItems &&
-                    <Image src={`/${arrow}.svg`} width="12" height="12" alt={alt} onClick={() => setIsOpen(!isOpen)}/>
+                    <div className="w-[12px] h-[12px] mt-1 md:w-[20px] md:h-[20px]">
+                        <Image
+                            src={`/${arrow}.svg`}
+                            width="12"
+                            height="12"
+                            alt={alt}
+                            onClick={() => setIsOpen(!isOpen)}
+                            className="w-full"
+                        />
+                    </div>
                 }
 
                 <Link href={linkTo}>
-                    <div className={`flex space-x-4 ${!subItems ? "ml-6": ""}`}>
-                        <Image src={imageSrc} width="23" height="23" alt={imageAltText}/>
-                        <h2>{label}</h2>
+                    <div className={`flex space-x-4 ${!subItems ? "ml-6 md:ml-9": ""}`}>
+                        <div className="w-[23px] h-[23px] md:w-[30px] md:h-[30px]">
+                            <Image
+                                src={imageSrc}
+                                width="23"
+                                height="23"
+                                alt={imageAltText}
+                                className="w-full"
+                            />
+                        </div>
+                        <h2 className="md:text-2xl">{label}</h2>
                     </div>
                 </Link>
             </div>
 
             {subItems && isOpen &&
-                <div className="mt-4 ml-16 pl-2 flex flex-col space-y-2.5 border border-neutral-300 border-b-0 border-t-0 border-l-1 border-r-0">
+                <div className="mt-4 ml-16 md:ml-20 pl-2 flex flex-col space-y-2.5 border border-neutral-300 border-b-0 border-t-0 border-l-1 border-r-0">
                     {subItems.map((item) => <MenuSubItemLink label={item.label} linkTo={item.linkTo} key={item.label} />)}
                 </div>
             }
