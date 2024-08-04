97c2d11fac54c067204d681c7a87a35c7bcdb381
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-04 15:31:08 -0400

    add cleanout message on article delete

--- app/components/articles/DeleteArticleModal.tsx
+++ app/components/articles/DeleteArticleModal.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { Dispatch, SetStateAction } from "react";
 import ConfirmationModal from "@/app/components/modal/ConfirmationModal";
+import Link from "next/link";
+import * as React from "react";
 
 interface DeleteArticleModalProps {
     setIsOpen: Dispatch<SetStateAction<boolean>>
@@ -10,7 +12,11 @@ interface DeleteArticleModalProps {
 const DeleteArticleModal = ({ setIsOpen, handleSubmit }: DeleteArticleModalProps) => {
     return (
         <ConfirmationModal setIsOpen={setIsOpen} confirmAction={handleSubmit}>
-            <p className="text-center text-xl mt-14 mb-8 md:text-2xl">
+            <p className="self-center w-[80%] text-center text-base p-2 bg-neutral-200 rounded-lg md:text-xl">
+                Reminder that clothes aren't trash! Check out our <Link href="/cleanout/recommendations" className="text-theme-green"> cleanout recs </Link>
+                page for some suggestions on sustainable ways to clean out your closet.
+            </p>
+            <p className="text-center text-xl my-8 md:text-2xl">
                 Are you sure you want to delete this article from your closet?
             </p>
         </ConfirmationModal>
