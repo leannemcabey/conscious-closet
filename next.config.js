53052250728009b98a003b9aafa13add89f9cd36
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-10 17:28:23 -0400

    remove comment

--- next.config.js
+++ next.config.js
@@ -10,8 +10,6 @@ module.exports = async (phase) => {
 
     if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
         const withSerwist = (await import("@serwist/next")).default({
-            // Note: This is only an example. If you use Pages Router,
-            // use something else that works, such as "service-worker/index.ts".
             swSrc: "app/sw.ts",
             swDest: "public/sw.js",
         });
