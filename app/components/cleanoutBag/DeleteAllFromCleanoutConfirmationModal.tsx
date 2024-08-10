5f1d0d304621897cda930e15062215b7eb171c60
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-09 20:26:21 -0400

    add children prop to confirmationmodal

--- app/components/cleanoutBag/DeleteAllFromCleanoutConfirmationModal.tsx
+++ app/components/cleanoutBag/DeleteAllFromCleanoutConfirmationModal.tsx
@@ -12,20 +12,22 @@ interface DeleteConfirmationModalProps {
 const DeleteAllFromCleanoutConfirmationModal = ({ setIsDeleting, handleSubmit }: DeleteConfirmationModalProps) => {
     return (
         <ConfirmationModal setIsOpen={setIsDeleting} confirmAction={handleSubmit}>
-            <Image
-                unoptimized
-                src={"/warning-gold.gif"}
-                alt={"warning"}
-                width="125"
-                height="125"
-                className="self-center"
-            />
-            <p className="text-center text-lg mt-1 mb-2 md:text-2xl">
-                Are you sure?
-            </p>
-            <p className="text-center text-sm mb-8 md:text-lg">
-                Doing this will delete all cleanout bag articles (regardless of applied filters!) from your closet as well as from your clean out bag.
-            </p>
+            <>
+                <Image
+                    unoptimized
+                    src={"/warning-gold.gif"}
+                    alt={"warning"}
+                    width="125"
+                    height="125"
+                    className="self-center"
+                />
+                <p className="text-center text-lg mt-1 mb-2 md:text-2xl">
+                    Are you sure?
+                </p>
+                <p className="text-center text-sm mb-8 md:text-lg">
+                    Doing this will delete all cleanout bag articles (regardless of applied filters!) from your closet as well as from your clean out bag.
+                </p>
+            </>
         </ConfirmationModal>
     )
 }
