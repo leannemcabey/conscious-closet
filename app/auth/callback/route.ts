9736c5a9d54b059736ee0b20a698575ef290f9ed
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-10 12:31:36 -0400

    add log in auth callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,6 +3,7 @@ import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
     const { searchParams, origin } = new URL(request.url)
+    console.log(`origin: ${origin}`)
     const code = searchParams.get('code')
     // if "next" is in param, use it as the redirect URL
     const next = searchParams.get('next') ?? '/home'
