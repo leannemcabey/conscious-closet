8bb5544510a0a2022f896ff497301f672c41663a
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-11 14:26:10 -0400

    fix import

--- app/sw.ts
+++ app/sw.ts
@@ -1,6 +1,5 @@
-import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import {NetworkOnly, Serwist} from "serwist";
+import { NetworkOnly, Serwist } from "serwist";
 
 // This declares the value of `injectionPoint` to TypeScript.
 // `injectionPoint` is the string that will be replaced by the
