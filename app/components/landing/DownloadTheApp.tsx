48281a98360574fa2a60152ee12a71c46c8ea631
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-10 20:50:43 -0400

    re-styling

--- app/components/landing/DownloadTheApp.tsx
+++ app/components/landing/DownloadTheApp.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 
 const DownloadTheApp = () => {
     return (
-        <div className="bg-white border border-theme-green p-4 rounded-lg flex flex-col justify-center items-center text-center">
+        <div className="bg-white border border-theme-green p-4 rounded-lg flex flex-col justify-center items-center text-center md:p-6">
             <div className="w-[40px] h-[40px] md:w-[80px] md:h-[80px]">
                 <Image
                     src={`/conscious-closet-logo.svg`}
@@ -13,23 +13,10 @@ const DownloadTheApp = () => {
                     className="w-full"
                 />
             </div>
-            <p className="text-lg mt-2 mb-8 md:mb-12 md:text-3xl">Download the app!</p>
+            <p className="text-lg text-theme-green tracking-widest mt-4 mb-8 md:mb-12 md:text-3xl">Download the app</p>
             <div className="text-base space-y-2 md:space-y-4 md:text-2xl">
                 <div className="flex space-x-1 items-center">
-                    <div className="w-[30px] h-[30px] md:w-[40px] md:h-[40px]">
-                        <Image
-                            src={`/download-arrow.svg`}
-                            height="20"
-                            width="20"
-                            alt="download icon"
-                            className="w-full"
-                        />
-                    </div>
-                    <p>From your web browser search bar</p>
-                </div>
-
-                <div className="flex space-x-1 items-center">
-                    <div className="w-[30px] h-[30px] md:w-[40px] md:h-[40px]">
+                    <div className="w-[40px] h-[40px] md:w-[60px] md:h-[60px]">
                         <Image
                             src={`/add-to-homescreen.svg`}
                             height="20"
@@ -38,10 +25,12 @@ const DownloadTheApp = () => {
                             className="w-full"
                         />
                     </div>
-                    <p>Add to Home screen option on mobile</p>
+                    <div className="text-left">
+                        <p>Add to Home screen from the triple</p>
+                        <p>dot menu of your mobile browser</p>
+                    </div>
                 </div >
             </div>
-            <p className="mt-8 text-xs text-neutral-500 md:text-lg md:mt-12">App store download coming soon!</p>
         </div>
     )
 }
