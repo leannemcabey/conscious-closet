444a5d672f61b01fbd3c8d0507289e0da18ecbc6
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-12 17:54:40 -0400

    remove bg image from offline page

--- app/components/OfflineContainer.tsx
+++ app/components/OfflineContainer.tsx
@@ -3,9 +3,7 @@ import * as React from "react";
 
 const OfflineContainer = () => {
     return (
-        <div className="flex justify-center min-h-screen max-h-screen relative">
-            <Image priority src="/pexels-liza-summer-closet.jpg" fill style={{opacity: 0.2}}
-                   alt="Photo of clothing on hangers by Liza Summer from Pexels"/>
+        <div className="bg-background-green flex justify-center min-h-screen max-h-screen relative">
             <div className="w-[80%] flex flex-col items-center self-center text-center text-text-green">
                 <div className="w-[200px] md:w-[350px]">
                     <Image src="/cry.svg" width="200" height="200" alt="sad" className="w-full"/>
