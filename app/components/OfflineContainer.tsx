9f85b884335123064bda592b42cf851c63e3c751
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-12 17:56:52 -0400

    remove unnecessary css class

--- app/components/OfflineContainer.tsx
+++ app/components/OfflineContainer.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 
 const OfflineContainer = () => {
     return (
-        <div className="bg-background-green flex justify-center min-h-screen max-h-screen relative">
+        <div className="bg-background-green flex justify-center min-h-screen max-h-screen">
             <div className="w-[80%] flex flex-col items-center self-center text-center text-text-green">
                 <div className="w-[200px] md:w-[350px]">
                     <Image src="/cry.svg" width="200" height="200" alt="sad" className="w-full"/>
