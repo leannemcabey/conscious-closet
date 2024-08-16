22ab88c48442277df4b9eee2eff09ff68316febf
leannemcabey <leanne.m.cabey@gmail.com>
2024-08-15 20:23:12 -0400

    view all elements smaller

--- app/components/capsuleCreator/AllElementsView.tsx
+++ app/components/capsuleCreator/AllElementsView.tsx
@@ -10,14 +10,14 @@ interface AllElementsViewProps {
 
 const AllElementsView = ({ capsuleElements, updateExpandedElement }: AllElementsViewProps) => {
     return (
-        <div className="h-[77%] grid grid-cols-2 grid-rows-3 justify-items-center md:grid-cols-3 md:grid-rows-2 md:mt-12">
+        <div className="h-[75%] grid grid-cols-2 grid-rows-3 justify-items-center md:grid-cols-3 md:grid-rows-2 md:mt-12">
             {Array.from(capsuleElements.values()).map((element) => {
                 return (
                     <CapsuleElement
                         key={element.slot}
                         element={element}
                         updateExpandedElement={updateExpandedElement}
-                        sizeStyling= "w-[125px] max-h-[187px] md:w-[200px] md:max-h-[299px]"
+                        sizeStyling= "w-[110px] max-h-[187px] md:w-[200px] md:max-h-[299px]"
                         iconPositioning="bottom-[18%] left-[72%] md:bottom-[55px]"
                     />
                 )
